46e7bdcedf91f5c63cab30639c6bdeee9f3582e2
ism49186163 <ism49186163@a05.informatica.escoladeltreball.org>
2018-02-01 13:11:31 +0100

    version 2

--- mayork03.py
+++ mayork03.py
@@ -2,6 +2,6 @@
 numero1=input("Introduce un número:")
 numero2=input("Introduce otro número:")
 while numero1<numero2:
-	numero2=input("Introduce un numero mayor que")+str(numero2)+(":")
-print "El",numero2,"no es mayor que",numero1
+	numero1=input("Introduce un numero mayor:")
+
 	
--- nveces01.py
+++ nveces01.py
@@ -1,9 +1,15 @@
 #coding: utf-8
 x=input("Introduce un numero:")
-i=0
-while (i<x):
-	i+=1
-	print (input("Dime un numero"))
+cont=0
+suma=0
+while (cont<x):
+	aux=input("Introduce un numero:")
+	if aux>0:
+		cont+=1
+		suma=suma+aux
+print "Se han introducido",cont,"numeros"
+print "Y la suma de esos numeros es:",suma
+	
 
 	
 	
