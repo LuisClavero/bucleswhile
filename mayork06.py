281f26885e75452357c8a16430615e9bc7eb0150
ism49186163 <ism49186163@a05.informatica.escoladeltreball.org>
2018-02-01 13:51:13 +0100

    version actualizada

--- mayork06.py
+++ mayork06.py
@@ -1,11 +1,13 @@
 #coding: utf-8
 x=input("introduce un numero")
+if x<0:
+	x=input("introduce un numero")
 cont=0
 suma=0
-while (cont<x):
-    aux=input("introduce otro numero")
-    if aux>0:
-        cont+=1
-        suma=suma+aux
-	if suma>=x:
-            print "Programa terminado"
+while (cont<x):		
+	aux=input("introduce otro numero")
+	if aux>0:
+        	cont+=1
+		suma=suma+aux
+		if suma>=x:
+            		print "Programa terminado"
