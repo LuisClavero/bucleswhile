abf9400dcc95770d7adb18c96c0272cdc5836a19
ism49186163 <ism49186163@a05.informatica.escoladeltreball.org>
2018-02-08 12:35:29 +0100

    Version actualizada

--- mayork01.py
+++ mayork01.py
@@ -2,6 +2,6 @@
 numero1=input("Introduce un número:")
 numero2=input("Introduce un número:")
 while (numero1>numero2):
-	numero2=input("Inténtalo de nuevo")
+	numero2=input("Inténtalo de nuevo:")
 print ("Es mayor")
 
--- mayork02.py
+++ mayork02.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 numero1=float(input("Introduce un número:"))
-numero2=input("Introduce un número:")
+numero2=float(input("Introduce un número:"))
 while (numero2>numero1):
 	numero2=input("Inténtalo de nuevo:")
 print "El",numero1,"es mayor"
