cdc45b38a8dfc2593a16f2cc02604556b31a3338
ism49186163 <ism49186163@a05.informatica.escoladeltreball.org>
2018-02-08 13:14:18 +0100

    Version actualizada

--- mayork06.py
+++ mayork06.py
@@ -5,6 +5,6 @@ while limite<=0:
     limite=float(input("El límite debe ser mayor que 0. Inténtelo de nuevo:"))
 while suma<limite:
     aux= float(input("Escriba otro número:"))
-    suma=suma+aux
+    suma+=aux
 print "Ha superado el límite. La suma es:",suma
 print "Programa terminado."
